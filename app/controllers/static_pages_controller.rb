85a631d6dc565b18e11d27bd1d10dbce4c352017
viraffe <viraffidae@gmail.com>
2016-12-17 15:56:10 +0000

    Finish(?) static pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,15 @@
 class StaticPagesController < ApplicationController
+  
+  def about
+  end
+  
   def home
   end
 
   def help
   end
+  
+  def contact
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  
+  get 'static_pages/contact'
   get 'static_pages/home'
-
   get 'static_pages/help'
-
-  root 'application#hello'
+  get 'static_pages/about'
+  root 'static_pages#home'
 end
